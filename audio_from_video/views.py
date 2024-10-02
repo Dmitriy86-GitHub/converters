5338e480531a8705b98f83adf9e77a5910c8b2db
Dmitriy <b8ea5t@mail.ru>
2024-10-02 16:46:36 +0300

    Second commit

--- audio_from_video/views.py
+++ audio_from_video/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 from . forms import VideoUploadForm
-from django.http import HttpResponseRedirect
+import moviepy.editor
+
+
+
+
 # Create your views here.
 def home_page(request):
     return render(request, 'audio_from_video/homepage.html')
 
-# def audio_from_video(request):
-#     return render(request, 'audio_from_video/audio_from_video.html' )
 
 def storag_file(file):
-    with open('video_files/{file.name}','wb+') as new_file:
+    with open('audio_from_video/video_files/my_file','wb+') as new_file:
         for chunk in file.chunks():
             new_file.write(chunk)
 
@@ -22,5 +24,9 @@ class VideoView(View):
         form = VideoUploadForm(request.POST,request.FILES)
         if form.is_valid():
             storag_file(form.cleaned_data['video'])
+            video = moviepy.editor.VideoFileClip('audio_from_video/video_files/my_file')
+            audio = video.audio
+            audio.write_audiofile('audio_from_video/audio_files/my_audio.mp3')
             # return HttpResponseRedirect('/')
         return render(request, 'audio_from_video/audio_from_video.html',{'form':form})
+
